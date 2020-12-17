67db087d68dc17836499d00ff6f81f0bf76e29d6
kingredsiri <kingredsiri@gmail.com>
2020-12-17 08:29:35 -0600

    day 3 Finished

--- source/repos/AOC/Day's Code/day1.cpp
+++ source/repos/AOC/Day's Code/day1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
+#include <string>
 
 using namespace std;
 
--- source/repos/AOC/Day's Code/day3.cpp
+++ source/repos/AOC/Day's Code/day3.cpp
@@ -6,9 +6,11 @@
 
 using namespace std;
 
+int day3Helper(std::string*, int, int, std::string, int);
+
 void day3() {
 	string filename = "Input Files/day3.txt";
-	int slope = 3;
+	int x = 3, y = 1;
 	int lengthOfFile = 0, lengthOfString = 0;
 	ifstream input;
 	input.open(filename);
@@ -19,7 +21,6 @@ void day3() {
 	}
 	input.close();
 	input.open(filename);
-	int replica = ceil((lengthOfFile / (lengthOfString / slope)));
 	string* arr = new string[lengthOfFile];
 	for (int i = 0; i < lengthOfFile; i++) {
 		getline(input, temp);
@@ -28,18 +29,33 @@ void day3() {
 	input.close();
 
 	string final = arr[lengthOfFile - 1];
-	int t = 0, placement = 0;
-	int trees = 0;
-	while (arr[t] != final) {
-		if ((placement + slope) > arr[t].length()) {
-			placement = 0;
-		}
-		if (arr[t + 1].at(placement + slope) == '#') {
-			trees++;
-		}
-		t++;
-		placement += slope;
+	int trees = day3Helper(arr, x, y, final, lengthOfFile);
+	int slopes_X[5] = {1, 3, 5, 7, 1};
+	int slopes_Y[5] = {1, 1, 1, 1, 2};
+	long long int mul = 1;
+	for(int i = 0; i < 5; i++) {
+		int temp = day3Helper(arr, slopes_X[i], slopes_Y[i], final, lengthOfFile-1); 
+		mul *= temp;
 	}
-	printf("Trees: %d\n", trees);
+	printf("Day 3 Solution\n");
+	printf("Part 1 Solution: %d\n", trees);
+	cout << "Part 2 Solution: " << mul << endl;
 	delete[] arr;
+}
+
+int day3Helper(string* arr, int x, int y, string compare, int length) { 
+	int t = 0, placement = 0, trees = 0;
+	while (arr[t] != compare) {
+		if ((placement + x) >= arr[t].length()) {
+			placement = ((placement) - arr[t].length());
+		}
+		if((t+y) <= length) {
+			if (arr[t + y].at(placement + x) == '#') {
+				trees++;
+			}
+		}
+		t += y;
+		placement += x;
+	}
+	return trees;
 }
